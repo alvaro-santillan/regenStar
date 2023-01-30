363fdd6a6637261a6eeb3a652c03db1075f5ef56
Álvaro Santillan <31286929+alvaro-santillan@users.noreply.github.com>
2023-01-29 18:46:05 -0700

    RegenStar

---
+++ Regen.xcframework/ios-arm64_x86_64-simulator/Regen.framework/Headers/Regen.h
@@ -0,0 +1,18 @@
+//
+//  Regen.h
+//  Regen
+//
+//  Created by Regen on 1/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Regen.
+FOUNDATION_EXPORT double RegenVersionNumber;
+
+//! Project version string for Regen.
+FOUNDATION_EXPORT const unsigned char RegenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Regen/PublicHeader.h>
+
+
